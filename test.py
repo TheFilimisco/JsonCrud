1917aa2f8e861783b70bc6e4d4fb73d9f2ff2ae3
Steven Uriarte <54910978l@iespoblenou.org>
2025-01-09 12:36:16 +0100

    add function

--- test.py
+++ test.py
@@ -4,3 +4,10 @@ with open("data/frieda.json", mode="r", encoding="utf-8") as file:
     data = json.load(file)
 
 print(data["name"])
+print(type(data))
+
+print(data["hobbies"][1])
+
+#Acces to Json
+print(data["friends"][1]["hobbies"][0])
+
---
+++ utils.py
@@ -0,0 +1,7 @@
+import  json
+with open("data/frieda.json", mode="r", encoding="utf-8") as file:
+    data = json.load(file)
+
+def simplify(data, field, data_filer):
+    data["field"]
+    pass
